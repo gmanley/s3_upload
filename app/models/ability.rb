afab6778d524194ad74dde94f34ebac079b1bbaa
Gray Manley <gray.manley@gmail.com>
2012-04-11 09:46:35 -0500

    Improve permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user
-      can :manage, Album, user_id: user.id
-      can :manage, Image, album: { user_id: user.id }
-      can :read, :all
-      cannot :read, Album, private: true
-      can :read, Album, private: true, user_id: user.id
-    else
-      can :read, :all
-      cannot :read, Album, private: true
+    if user # Registered user?
+      can :manage, Album, user_id: user.id # Can manage any album they own
+      can :manage, Image, album: { user_id: user.id } # Can manage any image in an album they own
+      can :manage, User, id: user.id # Can umm manage themselves??
+      can :read, :all # Can read anything ...
+      cannot :read, Album, private: true # except albums that are private ...
+      can :read, Album, private: true, user_id: user.id # unless they own the album
+    else # Guest
+      can :create, User # Can register a user
+      can :read, :all # Can read anything ...
+      cannot :read, Album, private: true # except albums that are private
+      cannot :read, Image, album: { private: true } # or images whose album is private
     end
   end
 end
